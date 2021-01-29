3a6282d3ff2e7811c0033916411039c1251d6d8e
lenaMihnuk <almikhniuk@gmail.com>
2021-01-29 12:32:17 +0300

    remove initState, styling screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:volyaApp/screen/music_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,20 +9,32 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => MusicScreen(),
-        ),
-      );
-    });
-    super.initState();
-  }
+  // void initState() {
+  //   Future.delayed(Duration(seconds: 3), () {
+  //     Navigator.of(context).pushReplacement(
+  //       MaterialPageRoute(
+  //         builder: (context) => MusicScreen(),
+  //       ),
+  //     );
+  //   });
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'My kid\'s app',
+            style: GoogleFonts.acme(
+              fontSize: 40,
+              color: Colors.purple,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.lime[100],
+      ),
       body: Image.asset(
         //add animation
         'assets/images/Olya.jpg',
